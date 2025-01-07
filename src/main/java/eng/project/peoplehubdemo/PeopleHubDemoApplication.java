962263c7da0782eae3418b72201f9a10ac1adc9b
Maciej G <146995602+mattTheSenior@users.noreply.github.com>
2025-01-07 11:14:39 +0100

    Initial commit

---
+++ src/main/java/eng/project/peoplehubdemo/PeopleHubDemoApplication.java
@@ -0,0 +1,13 @@
+package eng.project.peoplehubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleHubDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeopleHubDemoApplication.class, args);
+    }
+
+}
